a5ce9950278f900ae1f25ca523c8800a122844a0
Kendra Kyndberg <kendrak922@gmail.com>
2018-09-09 13:33:27 -0500

    flex

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -6,9 +6,13 @@ import picture from "../../images/car.jpg"
 
 
 const Projects = props => (
+<div>
 <div className="container">
+<div className="work">
 <img className="pic" src={picture} alt={props.image} />
 <p>{props.title}</p>
+</div>
+</div>
 </div>
 
 )
--- src/components/Wrapper/wrapper.js
+++ src/components/Wrapper/wrapper.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './style.scss'
 
-const Wrapper = props => <div className="container">{props.children}</div>
+const Wrapper = props => 
+<div className="container">
+<h1>Stuff I've Worked On</h1>
+{props.children}
+</div>
 
 export default Wrapper;
